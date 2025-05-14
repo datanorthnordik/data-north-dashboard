1e566fa983eeb9e3b2249fb530ee376b7dc004ae
athul-narayanan <athulnarayanan62@gmail.com>
2025-05-14 08:58:40 -0400

    contact us page enhanced

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -9,6 +9,14 @@ interface ContactUsProps {
 
 const ContactUs = (props: ContactUsProps) => {
     const { handleDrawerToggle } = props
+
+    const onJoinHere = ()=>{
+        window.open('https://forms.monday.com/forms/1b4f6c260a6c3f24010ae7e9d5414a5c?r=use1', '_blank', 'noopener,noreferrer'); 
+    }
+
+    const onSubscribeHere = ()=>{
+        window.open('https://nordikinstitute.us20.list-manage.com/subscribe?u=2a964e9b9625856f14a62b0c0&id=b20ef6b398', '_blank', 'noopener,noreferrer'); 
+    }
     return (
         <>
             <AppToolbar handleDrawerToggle={handleDrawerToggle} />
@@ -39,6 +47,16 @@ const ContactUs = (props: ContactUsProps) => {
                         </span>
                     </div>
                     <div className="contactus_subscribe">
+                        <p className="contactus_subscribe_para">
+                            Subscribe to NORDIK Institute's email list! Stay up to date with our 
+                            latest news and events including Spark series, press releases/news updates, 
+                            and monthly research spotlights.
+                        </p>
+                        <button onClick={onSubscribeHere} className="contactus_subscribe_button">Subscribe Here</button>
+                        <p className="contactus_subscribe_para">
+                            Interested in becoming a member of NORDIK Institute?
+                        </p>
+                        <button onClick={onJoinHere} className="contactus_subscribe_button">Join Here</button>
                     </div>
                 </div>
             </div>
